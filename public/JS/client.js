cdf075959fdb5fba8c4f7f2e17ac85ee1bb1ea35
BornPsych <yogeshshahi120@gmail.com>
2021-10-24 04:29:17 +0530

    commit 5

--- public/JS/client.js
+++ public/JS/client.js
@@ -1,121 +1,121 @@
 
-// window.onload = function () {
-    let socket = io('/');
-    // const documentId = new URL(window.location.href).pathname.split('/')[1] || 'test';
-    // // console.log(documentId);
+window.onload = function () {
+    let socket ;
+    const documentId = new URL(window.location.href).pathname.split('/')[1] || 'test';
+    // console.log(documentId);
     
-    // const handle = document.getElementById('handle');
-    // const register = document.getElementById('register');
+    const handle = document.getElementById('handle');
+    const register = document.getElementById('register');
     
-    // const editor = document.getElementById('editor');
-    // const textarea = document.getElementById('textarea');
-    // var Codeeditor = CodeMirror.fromTextArea(document.getElementById("textarea"), {
-    //             styleActiveLine: true,
-    //             lineNumbers: true,
-    //             matchBrackets: true
-    //         }); 
-    // console.log(editor)
+    const editor = document.getElementById('editor');
+    const textarea = document.getElementById('textarea');
+    var Codeeditor = CodeMirror.fromTextArea(document.getElementById("textarea"), {
+                styleActiveLine: true,
+                lineNumbers: true,
+                matchBrackets: true
+            }); 
+    console.log(editor)
 
-    // let syncValue = Array();
-    // let keypressed = false;
+    let syncValue = Array();
+    let keypressed = false;
 
-    // function addEditor(writer) {
-    //     var ul = document.getElementById("editors");
-    //     var li = document.createElement("li");
-    //     li.appendChild(document.createTextNode(writer.name));
-    //     li.className = "list-group-item";
-    //     li.id = writer.id;
-    //     ul.appendChild(li);
-    // }
+    function addEditor(writer) {
+        var ul = document.getElementById("editors");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(writer.name));
+        li.className = "list-group-item";
+        li.id = writer.id;
+        ul.appendChild(li);
+    }
 
-    // function removeElement(id) {
-    //     var elem = document.getElementById(id);
-    //     return elem.parentNode.removeChild(elem);
-    // }
-    // function applyLocalChanges() {
-    //     if (keypressed) {
-    //         let currentData = Codeeditor.getValue();
-    //         // console.log(currentData);
-    //         let input = Array.from(syncValue);
-    //         let output = Array.from(currentData);
-    //         let changes = getChanges(input, output);
-    //         applyChanges(input, changes);
-    //         if (output.join('') == input.join('')) {
-    //             socket.emit('content_change', {
-    //                 documentId: documentId,
-    //                 changes: changes
-    //             });
-    //             syncValue = input;
-    //         }
-    //         keypressed = false;
-    //     }
-    // }
-    // function setSocketEvents() {
-    //     socket.on('content_change', (incomingChanges) => {
-    //         let input = Array.from(syncValue);
-    //         applyChanges(input, incomingChanges);
-    //         syncValue = input;
-    //         applyLocalChanges();
+    function removeElement(id) {
+        var elem = document.getElementById(id);
+        return elem.parentNode.removeChild(elem);
+    }
+    function applyLocalChanges() {
+        if (keypressed) {
+            let currentData = Codeeditor.getValue();
+            // console.log(currentData);
+            let input = Array.from(syncValue);
+            let output = Array.from(currentData);
+            let changes = getChanges(input, output);
+            applyChanges(input, changes);
+            if (output.join('') == input.join('')) {
+                socket.emit('content_change', {
+                    documentId: documentId,
+                    changes: changes
+                });
+                syncValue = input;
+            }
+            keypressed = false;
+        }
+    }
+    function setSocketEvents() {
+        socket.on('content_change', (incomingChanges) => {
+            let input = Array.from(syncValue);
+            applyChanges(input, incomingChanges);
+            syncValue = input;
+            applyLocalChanges();
 
-    //         // let ranges = editor.getSelection().getRanges();
-    //         Codeeditor.setValue(syncValue.join(''))
-    //         // editor.getSelection().selectRanges(ranges);
-    //     });
-    //     socket.on('register', (data) => {
-    //         addEditor(data);
-    //     });
+            // let ranges = editor.getSelection().getRanges();
+            Codeeditor.setValue(syncValue.join(''))
+            // editor.getSelection().selectRanges(ranges);
+        });
+        socket.on('register', (data) => {
+            addEditor(data);
+        });
 
-    //     socket.on('user_left', (data) => {
-    //         removeElement(data.id);
-    //     });
-    //     socket.on('members', (members) => {
-    //         members.forEach(member => {
-    //             addEditor(member);
-    //         });
-    //         socket.off('members');
-    //     });
-    // }
+        socket.on('user_left', (data) => {
+            removeElement(data.id);
+        });
+        socket.on('members', (members) => {
+            members.forEach(member => {
+                addEditor(member);
+            });
+            socket.off('members');
+        });
+    }
 
-    // function registerUserListener() {
-    //     handle.style.display = 'none';
-    //     register.style.display = 'none';
+    function registerUserListener() {
+        handle.style.display = 'none';
+        register.style.display = 'none';
 
-    //     const editorBlock = document.getElementById('editor-block');
-    //     editorBlock.style.display = 'block';
-    //     syncValue = "";
-    //     socket = io();
-    //     socket.emit('register', {
-    //         handle: handle.value,
-    //         documentId: documentId
-    //     });
-    //     setSocketEvents();
-    // }
+        const editorBlock = document.getElementById('editor-block');
+        editorBlock.style.display = 'block';
+        syncValue = "";
+        socket = io();
+        socket.emit('register', {
+            handle: handle.value,
+            documentId: documentId
+        });
+        setSocketEvents();
+    }
 
-    // function getChanges(input, output) {
-    //     return diffToChanges(diff(input, output), output);
-    // }
+    function getChanges(input, output) {
+        return diffToChanges(diff(input, output), output);
+    }
 
-    // function applyChanges(input, changes) {
-    //     changes.forEach(change => {
-    //         if (change.type == 'insert') {
-    //             input.splice(change.index, 0, ...change.values);
-    //         } else if (change.type == 'delete') {
-    //             input.splice(change.index, change.howMany);
-    //         }
-    //     });
-    // }
+    function applyChanges(input, changes) {
+        changes.forEach(change => {
+            if (change.type == 'insert') {
+                input.splice(change.index, 0, ...change.values);
+            } else if (change.type == 'delete') {
+                input.splice(change.index, change.howMany);
+            }
+        });
+    }
 
-    // var timeout = setTimeout(null, 0);
-    // editor.addEventListener('keypress', () => {
-    //     clearTimeout(timeout);
-    //     keypressed = true;
-    //     timeout = setTimeout(applyLocalChanges, 1000);
-    // });
+    var timeout = setTimeout(null, 0);
+    editor.addEventListener('keypress', () => {
+        clearTimeout(timeout);
+        keypressed = true;
+        timeout = setTimeout(applyLocalChanges, 1000);
+    });
 
-    // register.addEventListener('click', registerUserListener);
+    register.addEventListener('click', registerUserListener);
 
 
-const ROOM_ID = new URL(window.location.href).pathname.split('/')[1] || 'test';
+const ROOM_ID = documentId;
 
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
@@ -269,4 +269,4 @@ socket.on("createMessage", (message, userName) => {
 
 
 
-// }
+}

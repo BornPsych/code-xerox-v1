95eecc2c98405fa7319f2108503dd47254063c6c
BornPsych <yogeshshahi120@gmail.com>
2021-10-25 09:13:17 +0530

    commit 15

--- public/JS/client.js
+++ public/JS/client.js
@@ -170,7 +170,8 @@ window.onload = function () {
         });
     
         socket.on("user-connected", (userId) => {
-          connectToNewUser(userId, stream);
+          // connectToNewUser(userId, stream);
+          setTimeout(connectToNewUser,1000,userId,stream)
         });
       });
     
@@ -220,12 +221,12 @@ window.onload = function () {
       const enabled = myVideoStream.getAudioTracks()[0].enabled;
       if (enabled) {
         myVideoStream.getAudioTracks()[0].enabled = false;
-        html = `<i class="fas fa-microphone-slash"></i>`;
+        let html = `<i class="fas fa-microphone-slash"></i>`;
         muteButton.classList.toggle("background__red");
         muteButton.innerHTML = html;
       } else {
         myVideoStream.getAudioTracks()[0].enabled = true;
-        html = `<i class="fas fa-microphone"></i>`;
+        let html = `<i class="fas fa-microphone"></i>`;
         muteButton.classList.toggle("background__red");
         muteButton.innerHTML = html;
       }
@@ -235,12 +236,12 @@ window.onload = function () {
       const enabled = myVideoStream.getVideoTracks()[0].enabled;
       if (enabled) {
         myVideoStream.getVideoTracks()[0].enabled = false;
-        html = `<i class="fas fa-video-slash"></i>`;
+        let html = `<i class="fas fa-video-slash"></i>`;
         stopVideo.classList.toggle("background__red");
         stopVideo.innerHTML = html;
       } else {
         myVideoStream.getVideoTracks()[0].enabled = true;
-        html = `<i class="fas fa-video"></i>`;
+        let html = `<i class="fas fa-video"></i>`;
         stopVideo.classList.toggle("background__red");
         stopVideo.innerHTML = html;
       }
--- src/server.js
+++ src/server.js
@@ -83,11 +83,12 @@ io.on("connection", socket => {
     });
 
      socket.on("join-room", (roomId, userId, userName) => {
+        console.log(roomId,userId, userName);
         socket.join(roomId);
         socket.broadcast.to(roomId).emit("user-connected", userId);
         socket.on("message", (message) => {
         io.to(roomId).emit("createMessage", message, userName);
-        console.log(userName)
+        console.log(message);
         });
     });
 
